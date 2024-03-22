e7d52e1a1d6f2b7649a6f949051b2db4869eeca3
ekhassan <ek.hassan.07@gmail.com>
2024-03-22 11:11:02 +0500

    Shadow and Card Scheme

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -31,10 +31,11 @@
             /* max-width: 340px; */
             min-height: 13.438rem !important;
             max-height: 13.438rem !important;
+            box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
         }
 
         .card-body {
-            background-image: url('{{ asset('/images/mask.png') }}');
+            background-image: url('{{ asset('/images/companyCard.png') }}');
             background-size: cover;
             background-position: center;
             min-height: 13.438rem !important;
